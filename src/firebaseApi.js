50fd1ff5582f9da8788da101dfc7335954a81a0a
Nikolay Boldar <nboldar1@gmail.com>
2019-06-24 13:34:10 +0300

    some issue with api

---
+++ src/firebaseApi.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyBUsTb2CQB9QHP44BjHmiMCl6YP33h8Dnk',
+  authDomain: 'vue-app-fire.firebaseapp.com',
+  databaseURL: 'https://vue-app-fire.firebaseio.com',
+  projectId: 'vue-app-fire',
+  storageBucket: 'vue-app-fire.appspot.com',
+  messagingSenderId: '1066036337706',
+  appId: '1:1066036337706:web:133c81394e0cd760',
+};
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue';
 import router from './router/router';
 import store from './store/store';
 import AppBuyModel from './components/shared/BuyModal.vue';
+import fbApi from './firebaseApi';
 
 Vue.component('app-buy-modal', AppBuyModel);
 Vue.config.productionTip = false;
@@ -14,15 +15,7 @@ new Vue({
   store,
   render: h => h(App),
   created() {
-    fb.initializeApp({
-      apiKey: 'AIzaSyBUsTb2CQB9QHP44BjHmiMCl6YP33h8Dnk',
-      authDomain: 'vue-app-fire.firebaseapp.com',
-      databaseURL: 'https://vue-app-fire.firebaseio.com',
-      projectId: 'vue-app-fire',
-      storageBucket: 'vue-app-fire.appspot.com',
-      messagingSenderId: '1066036337706',
-      appId: '1:1066036337706:web:133c81394e0cd760',
-    });
+    fb.initializeApp(fbApi);
     fb.auth().onAuthStateChanged((user) => {
       if (user) {
         this.$store.dispatch('autoLoginUser', user);
